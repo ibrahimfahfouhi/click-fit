7054a9d183b14ff0b964dd4c045abc0bd74aa136
Fahfouhi <fahfouhi@gmail.com>
2025-02-01 17:10:36 +0100

    Click-Fit-Backend Project Setup

---
+++ click-fit-backend/src/main/java/ma/javatiyoun/clickfitbackend/ClickFitBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.javatiyoun.clickfitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickFitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickFitBackendApplication.class, args);
+    }
+
+}
---
+++ click-fit-backend/src/test/java/ma/javatiyoun/clickfitbackend/ClickFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.javatiyoun.clickfitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickFitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
